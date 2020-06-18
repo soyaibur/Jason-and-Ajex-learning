e9ffcc32f42c48273c29cf4213ed5c68484ec63e
soyaibur <ovisoyaiburrahman@gmail.com>
2020-06-18 09:28:44 +0600

    loop learning

--- main.js
+++ main.js
@@ -1,8 +1,32 @@
-var ourRequest = new XMLHttpRequest();
-ourRequest.open('GET','https://learnwebcode.github.io/json-example/animals-1.json');
-ourRequest.onload = function(){
-  var ourData = JSON.parse(ourRequest.responseText);
-  console.log(ourData[0]);
-};
+var animalContainer = document.getElementById("animal-info");
 
-ourRequest.send();
+var btn = document.getElementById("button");
+btn.addEventListener("click",function(){
+ 
+  var ourRequest = new XMLHttpRequest();
+  ourRequest.open('GET','https://learnwebcode.github.io/json-example/animals-1.json');
+  ourRequest.onload = function(){
+    var ourData = JSON.parse(ourRequest.responseText);
+    randerHtml(ourData);
+  };
+  
+  ourRequest.send();
+
+});
+
+function randerHtml(data){
+  var htmlString = 
+  animalContainer.insertAdjacentHTML('beforeend',htmlString);
+}
+
+
+var ourHeadline = document.getElementById("kaka");
+var items = document.getElementById("our-list").getElementsByTagName("li");
+
+for(i=0; i < items.length; i++){
+  items[i].addEventListener("click",pado);
+}
+
+function pado(){
+   ourHeadline.innerHTML = this.innerHTML;
+};
