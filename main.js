1a448ef9d414a51d1b6040a3f9a0125cd30d65eb
soyaibur <ovisoyaiburrahman@gmail.com>
2020-06-26 05:25:57 +0600

    prectiov

--- main.js
+++ main.js
@@ -1,32 +1,6 @@
-var animalContainer = document.getElementById("animal-info");
-
-var btn = document.getElementById("button");
-btn.addEventListener("click",function(){
- 
-  var ourRequest = new XMLHttpRequest();
-  ourRequest.open('GET','https://learnwebcode.github.io/json-example/animals-1.json');
-  ourRequest.onload = function(){
-    var ourData = JSON.parse(ourRequest.responseText);
-    randerHtml(ourData);
-  };
-  
-  ourRequest.send();
-
-});
-
-function randerHtml(data){
-  var htmlString = 
-  animalContainer.insertAdjacentHTML('beforeend',htmlString);
+var ourRequest =new XMLHttpRequest();
+ourRequest.open('GET','https://learnwebcode.github.io/json-example/animals-1.json');
+ourRequest.onload = function(){
+   var ourData = JSON.parse(ourRequest.responseText);
 }
-
-
-var ourHeadline = document.getElementById("kaka");
-var items = document.getElementById("our-list").getElementsByTagName("li");
-
-for(i=0; i < items.length; i++){
-  items[i].addEventListener("click",pado);
-}
-
-function pado(){
-   ourHeadline.innerHTML = this.innerHTML;
-};
+ourRequest.send();
