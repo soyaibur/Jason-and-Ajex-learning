8877c0b762d540a26d3e99a82117e5b033c499a8
soyaibur <ovisoyaiburrahman@gmail.com>
2020-06-26 06:26:15 +0600

    das

--- main.js
+++ main.js
@@ -1,6 +1,22 @@
-var ourRequest =new XMLHttpRequest();
-ourRequest.open('GET','https://learnwebcode.github.io/json-example/animals-1.json');
-ourRequest.onload = function(){
-   var ourData = JSON.parse(ourRequest.responseText);
+var pageCounter = 1;
+var das = document.getElementById("animal-info");
+document.getElementById("button").addEventListener('click',function(){
+  var ourRequest =new XMLHttpRequest();
+  ourRequest.open('GET','https://learnwebcode.github.io/json-example/animals-'+ pageCounter +'.json');
+  ourRequest.onload = function(){
+     var ourData = JSON.parse(ourRequest.responseText);
+     renderHTML(ourData);
+  }
+  ourRequest.send();
+  pageCounter++;
+});
+
+function renderHTML(data){
+  var htmlString = "";
+
+  for (let i = 0; i < data.length; i++) {
+    htmlString += "<p>" + data[i].name +"is a " + data[i].species +".</p>";
+    
+  }
+ das.insertAdjacentHTML('beforeend', htmlString);
 }
-ourRequest.send();
